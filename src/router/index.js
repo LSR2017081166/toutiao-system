df2e8df56d5aa97e8e3474e7d741d1bf4d101ff0
小罗号 <1098863973@qq.com>
2021-10-11 11:39:37 +0800

    调整初始目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   // {
   //   path: '/about',
   //   name: 'About',
